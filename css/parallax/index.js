f2ec4c200628d6ee1400b85681046c4a3ba26662
romluc <romulofilhofilho@gmail.com>
2020-04-05 15:52:53 -0300

    Add dummy text

---
+++ css/parallax/index.js
@@ -0,0 +1,14 @@
+let bg = document.querySelector('#bg');
+let moon = document.querySelector('#moon');
+let mountain = document.querySelector('#mountain');
+let road = document.querySelector('#road');
+let text = document.querySelector('#text');
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+
+  bg.style.top = `${value * 0.5}px`;
+  moon.style.left = `${-value * 0.2}px`;
+  mountain.style.top = `${-value * 0.15}px`;
+  text.style.top = `${value}px`;
+});
